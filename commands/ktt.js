4b5161097d6b45de2b2583c818837eac89a288ea
faruto-natuno <133721317+faruto-natuno@users.noreply.github.com>
2023-05-16 19:36:17 +0900

    Update ktt.js

--- commands/ktt.js
+++ commands/ktt.js
@@ -4,6 +4,6 @@ module.exports = {
         description: "KTTを召喚します。",
     },
 	async execute(interaction) {
-        await interaction.reply('勉強なんか放っておいて、こっちこい<@1062565968959774761>')
+        await interaction.reply('井原: <@1062565968959774761>さん、まだ宿題が残っていますよ♪')
 	}
 }
