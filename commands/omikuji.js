45752eaca0220200a6afe5ee63ba0f2ba40c70ce
faruto-natuno <133721317+faruto-natuno@users.noreply.github.com>
2023-05-16 20:52:05 +0900

    URLの間違い修正

--- commands/omikuji.js
+++ commands/omikuji.js
@@ -4,7 +4,7 @@ module.exports = {
         description: "おみくじを引くことができます。",
     },
 	async execute(interaction) {
-        let arr = ["https://i.ibb.co/CwD67y5/5.png", "https://i.ibb.co/TqnpK7Q/4.png", "https://i.ibb.co/CP0P5n5/3.png", "https://i.ibb.co/GRhTSHh/image.png", "ttps://i.ibb.co/5rXXv7b/2.png", "https://i.ibb.co/W6y4YrN/1.png", "https://i.ibb.co/bBdQH47/image.png"];
+        let arr = ["https://i.ibb.co/CwD67y5/5.png", "https://i.ibb.co/TqnpK7Q/4.png", "https://i.ibb.co/CP0P5n5/3.png", "https://i.ibb.co/GRhTSHh/image.png", "https://i.ibb.co/5rXXv7b/2.png", "https://i.ibb.co/W6y4YrN/1.png", "https://i.ibb.co/bBdQH47/image.png"];
         let weight = [15, 25, 20, 20, 15, 10, 5];
         let totalWeight = 0;
         for (var i = 0; i < weight.length; i++){
