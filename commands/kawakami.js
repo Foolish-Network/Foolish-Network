fcbbcc47aee6ce62dccb96ddad0bbc97b241bab0
faruto-natuno <133721317+faruto-natuno@users.noreply.github.com>
2023-05-22 10:26:17 +0900

    不要ファイルの削除

--- commands/kawakami.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	data: {
-        name: "kensei",
-        description: "kawakamiを召喚します。",
-    },
-	async execute(interaction) {
-        await interaction.reply('<@1077158863884800110>って良いやつだよな・・')
-	}
-}
