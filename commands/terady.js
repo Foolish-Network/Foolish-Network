2a47ef613384641530dd031a18252f4eabd75e75
faruto-natuno <133721317+faruto-natuno@users.noreply.github.com>
2023-05-22 10:26:51 +0900

    不要ファイルの削除

--- commands/terady.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	data: {
-        name: "terady",
-        description: "Teradyを召喚します。",
-    },
-	async execute(interaction) {
-        await interaction.reply('<@1065599142019219477>、プロセカしようぜ！')
-	}
-}
