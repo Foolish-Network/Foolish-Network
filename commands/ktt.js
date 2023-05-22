740902623ff6ef816a46c1d5b237a311c2bab358
faruto-natuno <133721317+faruto-natuno@users.noreply.github.com>
2023-05-22 10:26:33 +0900

    不要ファイルの削除

--- commands/ktt.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	data: {
-        name: "ktt",
-        description: "KTTを召喚します。",
-    },
-	async execute(interaction) {
-        await interaction.reply('井原: <@1062565968959774761>さん、まだ宿題が残っていますよ♪')
-	}
-}
